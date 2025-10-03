c53a93fa83158288bfaed5ed7809a8231b572cc6
Aleks Sh <dragon3@mail.ru>
2025-10-03 15:43:44 +0700

    initial commit

---
+++ frontend/dashboard/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    Заголовок
+  </header>
+</template>
+
+<style scoped>
+header {
+  height: 2rem;
+  font-size: 2rem;
+}
+@media (min-width: 1024px) {
+}
+</style>
