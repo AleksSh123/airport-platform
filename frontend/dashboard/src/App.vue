9bd6e6bf0f966595139e4d66567ca2ac565eceec
alex <dragon3@mail.ru>
2025-10-09 00:02:03 +0700

    fixed update tasks list view

--- frontend/dashboard/src/App.vue
+++ frontend/dashboard/src/App.vue
@@ -3,7 +3,7 @@
   const PORT = Number(import.meta.env.VITE_TASK_PORT);
   const TASK_URL = `http://${HOST}:${PORT}/tasks`;
   const MODE = import.meta.env.MODE;
-  import { reactive } from 'vue';
+  import { reactive, ref } from 'vue';
   import TasksHeader from './components/TasksHeader.vue'
   import InputRow from './components/InputRow.vue';
   import OutputRow from './components/OutputRow.vue';
@@ -12,7 +12,7 @@
     service_type: "service1",
     status: "new",
   }
-  let tasks = reactive([]);
+  let tasks = ref([]);
   const newTask = reactive({
     id: "",
     order_item_id: "",
@@ -78,8 +78,8 @@
      await updateView();
   }
   async function updateView(){
-    tasks = await getTasks();
-    console.debug(`tasks is: ${JSON.stringify(tasks, null, 2)}`);
+    tasks.value = await getTasks();
+    console.debug(`tasks is: ${JSON.stringify(tasks.value, null, 2)}`);
   }
 
 </script>
